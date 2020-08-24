2d9e52341c762d4021ca8e4b07978e7b376fb026
Faith Magras <bdanielle092@gmail.com>
2020-08-23 19:56:03 -0500

    working on part4

--- grimoire/js/grimiore/main.js
+++ grimoire/js/grimiore/main.js
@@ -1,5 +1,41 @@
 function main() {
-  // Put your code here
+    const allSpells = [{
+            Name: "Turn enemy into a newt",
+            IsEvil: true,
+            EnergyRequired: 5.1
+        },
+        {
+            Name: "Conjure some gold for a local charity",
+            IsEvil: false,
+            EnergyRequired: 2.99
+        },
+        {
+            Name: "Give a deaf person the ability to heal",
+            IsEvil: false,
+            EnergyRequired: 12.2
+        },
+        {
+            Name: "Make yourself emperor of the universe",
+            IsEvil: true,
+            EnergyRequired: 100.0
+        },
+        {
+            Name: "Convince your relatives your political views are correct",
+            IsEvil: false,
+            EnergyRequired: 2921.5
+        }
+    ]
+    const makeGoodSpellBook = () => {
+        return allSpells
+    }
+    const makeEvilSpellBook = () => {
+        return allSpells
+    }
+    const goodBook = makeGoodSpellBook(allSpells)
+    const evilBook = makeEvilSpellBook(allSpells)
+    const spells = goodBook.push('Conjure some gold for a local charity')
+    console.log(goodBook)
+
 
 }
 
